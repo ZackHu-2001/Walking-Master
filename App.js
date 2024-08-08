59e0a2f06226d9e6df3e256e3000877ab45ba5d2
hanzhangpeng2000 <peng.han@northeastern.edu>
2024-08-07 17:36:24 -0700

    fixed navigation to notification center

--- App.js
+++ App.js
@@ -3,10 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BottomTabNavigator from './Navigation/BottomTabNavigator';
+import NotificationCenterScreen from './Screens/NotificationCenterScreen';
 import { PaperProvider } from 'react-native-paper';
 import SignUp from './Screens/SignUp';
 import LogIn from './Screens/LogIn';
-import BottomTabNavigator from './Navigation/BottomTabNavigator';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,12 +15,13 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="LogIn">
+        <Stack.Navigator>
           <Stack.Screen name="MainTabs" component={BottomTabNavigator} options={{ headerShown: false }} />
+          <Stack.Screen name="NotificationCenter" component={NotificationCenterScreen} />
           <Stack.Screen name="SignUp" component={SignUp} />
           <Stack.Screen name="LogIn" component={LogIn} />
         </Stack.Navigator>
-      </NavigationContainer>
+    </NavigationContainer>
     </PaperProvider>
   );
 }
