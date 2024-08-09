2e9279041a213d5deb685a4deb47ee5a65d468d6
Zixiang Hu <144851646+ZackHu-2001@users.noreply.github.com>
2024-08-08 20:51:13 -0700

    support basic modal

---
+++ Components/ModalContent/AddRoom.js
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const AddRoom = () => {
+  return (
+    <View>
+      <Text>Add Room</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default AddRoom;
--- Screens/GameBoardScreen.js
+++ Screens/GameBoardScreen.js
@@ -1,10 +1,33 @@
 import React, { useEffect, useContext, useState } from 'react';
-import { View, Text, ScrollView, StyleSheet, Dimensions } from 'react-native';
+import { View, Button, Text, ScrollView, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
 import GameCard from '../Components/GameCard';
 import { getGames, getUser, listenForGames } from '../Firebase/firestoreHelper';
+import FloatingActionButton from '../Components/FloatingActionButton';
+import { Modal } from 'react-native-paper';
+import NewGame from '../Components/ModalContent/NewGame';
+import AddRoom from '../Components/ModalContent/AddRoom';
 
 export default function GameBoardScreen({ navigation }) {
   const [games, setGames] = useState([]);
+  const [visible, setVisible] = React.useState(false);
+  const [modalContent, setModalContent] = React.useState(1);
+
+  const showModal = () => setVisible(true);
+  const hideModal = () => setVisible(false);
+
+  const editRoom = () => {
+
+  }
+
+  const addNewGame = () => {
+    setModalContent(1);
+    showModal();
+  }
+
+  const addRoom = () => {
+    setModalContent(0);
+    showModal();
+  }
 
   useEffect(() => {
     const unsubscribe = listenForGames((games) => {
@@ -16,17 +39,24 @@ export default function GameBoardScreen({ navigation }) {
   }, []);
 
   return (
-    <ScrollView contentContainerStyle={styles.scrollView} >
-      {
-        games.map((game, index) => (
-          <GameCard
-            key={index}
-            title={game['name']}
-            onPress={() => navigation.navigate('Game', { gameId: game['id'] })}
-          />
-        ))
-      }
-    </ScrollView>
+    <>
+      <ScrollView contentContainerStyle={styles.scrollView} >
+        {
+          games.map((game, index) => (
+            <GameCard
+              key={index}
+              title={game['name']}
+              onPress={() => navigation.navigate('Game', { gameId: game['id'] })}
+            />
+          ))
+        }
+      </ScrollView>
+      <FloatingActionButton addNewGame={addNewGame} addRoom={addRoom} editRoom={editRoom}/>
+
+      <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={styles.containerStyle} >
+        {modalContent ? <NewGame /> : <AddRoom />}
+      </Modal>
+    </>
   )
 
 }
@@ -35,5 +65,10 @@ const styles = StyleSheet.create({
   scrollView: {
     width: Dimensions.get('window').width - 20,
     padding: 10,
-  }
+  },
+  containerStyle: {
+    width: Dimensions.get('window').width - 40,
+    marginLeft: 20,
+    marginRight: 20,
+  },
 });
