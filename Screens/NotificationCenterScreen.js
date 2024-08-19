2c3b51b4a3c875435e8a687f360668d545b6393f
hanzhangpeng2000 <peng.han@northeastern.edu>
2024-08-19 16:38:17 -0700

    moved styles to notificationstyle.js

--- Screens/NotificationCenterScreen.js
+++ Screens/NotificationCenterScreen.js
@@ -8,6 +8,7 @@ import * as Notifications from 'expo-notifications';
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 import DateTimePicker from '@react-native-community/datetimepicker';
 import Context from '../Context/context';
+import styles from '../Styles/NotificationStyle';
 
 const NotificationCenterScreen = () => {
   const { user } = useContext(Context);
@@ -182,96 +183,5 @@ const NotificationCenterScreen = () => {
     </LinearGradient>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  innerContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 16,
-  },
-  titleContainer: {
-    width: '80%',
-    alignSelf: 'center',
-    marginBottom: 20, 
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#6A4C9C',
-  },
-  dropdownContainer: {
-    marginBottom: 60,
-    width: '80%',
-    alignSelf: 'center',
-  },
-  dropdown: {
-    backgroundColor: '#ffffff',
-    borderColor: '#7B90D2',
-  },
-  dropdownContainerStyle: {
-    backgroundColor: '#e5e5ff',  // for the dropdown list
-  },
-  textStyle: {
-    color: '#333333',
-    fontSize: 16,
-    fontFamily: Platform.OS === 'ios' ? 'Helvetica' : 'Roboto',
-  },
-  placeholderStyle: {
-    color: '#999999', 
-    fontSize: 16,
-  },
-  labelStyle: {
-    color: '#4E4F97', 
-    fontSize: 16,
-    fontFamily: Platform.OS === 'ios' ? 'Helvetica' : 'Roboto', 
-  },
-  selectedItemLabelStyle: {
-    color: '#6A4C9C',
-    fontWeight: 'bold',
-    fontFamily: Platform.OS === 'ios' ? 'Helvetica' : 'Roboto',
-  },
-  dateTimeContainer: {
-    marginBottom: 30,
-    alignItems: 'center',
-  },
-  buttonContainer: {
-    alignItems: 'center',
-  },
-  dateTimePicker: {
-    width: '100%',
-  },
-  button: {
-    backgroundColor: '#6A4C9C', 
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    borderRadius: 25, 
-    marginBottom: 10,
-    width: '80%',
-  },
-  buttonText: {
-    color: '#ffffff',  
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  confirmButton: {
-    backgroundColor: '#993399',  
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    borderRadius: 25, 
-    width: '80%',
-    alignSelf: 'center',
-  },
-  confirmButtonText: {
-    color: '#ffffff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
 
 export default NotificationCenterScreen;
